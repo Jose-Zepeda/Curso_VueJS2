af3716975b3b46430ad6e99eae91b33c758d897f
Jose Zepeda <jzepedad1@miumg.edu.gt>
2025-08-29 17:57:31 -0600

    Ejercicio Practico

--- src/main/java/org/example/index.js
+++ src/main/java/org/example/index.js
@@ -1,22 +1,20 @@
-var vm = new Vue({
-    el: '#demo',
+new Vue({
+    el: '#app',
     data: {
-        firstName: 'Foo',
-        lastName: 'Bar'
+        nombre: ''
     },
     computed: {
-        fullName: {
-            // getter
-            get: function () {
-                return this.firstName + ' ' + this.lastName
-            },
-            // setter
-            set: function (newValue) {
-                var names = newValue.split(' ')
-                this.firstName = names[0]
-                this.lastName = names[names.length - 1]
-            }
+        mensajeBienvenida() {
+            return this.nombre
+                ? `¡Bienvenido, ${this.nombre}!`
+                : '¡Bienvenido!';
+        }
+    },
+    methods: {
+        mensajeBienvenidaMetodo() {
+            return this.nombre
+                ? `¡Bienvenido, ${this.nombre}!`
+                : '¡Bienvenido!';
         }
     }
-})
-
+});
